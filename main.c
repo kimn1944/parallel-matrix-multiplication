9e95468d8dbc9596577681b539901201621e3e35
Nikita Kim <kimn1944@gmail.com>
2020-02-27 01:56:44 -0500

    script added, seg fault

--- main.c
+++ main.c
@@ -11,11 +11,15 @@ int ROW = 0;
 int COL = 0;
 
 void io(int argc, char **argv) {
-    if (argc < 3) {
-	printf("Usage: %s <number of threads> <name of file>\n", argv[0]);
+    /*
+    if (argc < 2) {
+	printf("Usage: <name of file>\n");
 	exit(0);
     }
-    char *filename = argv[2];
+    
+    char *filename = argv[1];
+    */
+    char *filename = "mat200x200.txt";
     FILE *fptr = fopen(filename, "r");
     if (fptr == NULL) { 
 	printf("Cannot open file\n");
@@ -35,6 +39,33 @@ void io(int argc, char **argv) {
     fclose(fptr);
 }
 
+void alloc(int size) {
+    MAT_PTR = (int*)malloc(size * sizeof(int));
+    RESULT = (int*)malloc(size * sizeof(int));
+}
+
+void result_to_file() {
+    FILE *fptr = fopen("result.txt", "w");
+    if (fptr == NULL) {
+	printf("Cannot open the file result.txt\n");
+	exit(0);
+    }
+    fprintf(fptr, "Resulting matrix:\n");
+    fprintf(fptr, "%dx%d", ROW, COL);
+    int i, j;
+    for (i = 0; i < ROW; i++) {
+	for (j = 0; j < COL; j++) {
+	    fprintf(fptr, "%d ", *(RESULT + i * COL + j));
+	}
+	fprintf(fptr, "\n");
+    }
+    fclose(fptr);
+}
+
+void pass(int arg) {
+    printf("passed : %d\n", arg);
+}
+
 int main(int argc, char *argv[]) {
     int numtasks, taskid, source, destination, msg_type; // MPI stuff
     int rows, average, modulo, offset;
@@ -46,8 +77,8 @@ int main(int argc, char *argv[]) {
     
     if (taskid == MASTER) {
 	io(argc, argv);
-	average = COL / numtasks;
-	modulo = COL % numtasks;
+	average = COL / (numtasks - 1);
+	modulo = COL % (numtasks - 1);
 	offset = 0;
 	msg_type = FROM_MASTER;
 	for (destination = 1; destination < numtasks; destination++) {
@@ -56,7 +87,7 @@ int main(int argc, char *argv[]) {
 	    MPI_Send(&rows, 1, MPI_INT, destination, msg_type, MPI_COMM_WORLD);
 	    MPI_Send(&offset, 1, MPI_INT, destination, msg_type, 
 		    MPI_COMM_WORLD);
-	    MPI_Send(&(MAT_PTR + offset), rows * COL, MPI_INT, destination, 
+	    MPI_Send(&MAT_PTR[offset * COL], rows * COL, MPI_INT, destination, 
 		    msg_type, MPI_COMM_WORLD);
 	    offset += rows;
 	}
@@ -67,21 +98,23 @@ int main(int argc, char *argv[]) {
 		    &status);    
 	    MPI_Recv(&offset, 1, MPI_INT, source, msg_type, MPI_COMM_WORLD, 
 		    &status);
-	    MPI_Recv(&(RESULT + offset), rows * COL, MPI_INT, source, 
+	    MPI_Recv(&RESULT[offset * COL], rows * COL, MPI_INT, source, 
 		    msg_type, MPI_COMM_WORLD, &status);
-
 	}
+	result_to_file();
     } else {
 	msg_type = FROM_MASTER;
 	MPI_Recv(&COL, 1, MPI_INT, MASTER, msg_type, MPI_COMM_WORLD, &status);
+	alloc(COL * COL);
 	MPI_Recv(&rows, 1, MPI_INT, MASTER, msg_type, MPI_COMM_WORLD, &status);
 	MPI_Recv(&offset, 1, MPI_INT, MASTER, msg_type, MPI_COMM_WORLD,
 	       	&status);
+	pass(offset);
 	MPI_Recv(&MAT_PTR, rows * COL, MPI_INT, MASTER, msg_type, 
 		MPI_COMM_WORLD, &status);
 
 	int i, j, k;
-	for (i = 0; i < rows, i++) {
+	for (i = 0; i < rows; i++) {
 	    for (j = 0; j < COL; j++ ) {
 		*(RESULT + i * COL + j) = 0;
 		for (k = 0; k < COL; k++) {
@@ -91,11 +124,12 @@ int main(int argc, char *argv[]) {
 	    }
 	}
 
-	msg_type FROM_WORKER;
+	msg_type = FROM_WORKER;
 	MPI_Send(&rows, 1, MPI_INT, MASTER, msg_type, MPI_COMM_WORLD);
 	MPI_Send(&offset, 1, MPI_INT, MASTER, msg_type, MPI_COMM_WORLD);
 	MPI_Send(&RESULT, 1, MPI_INT, MASTER, msg_type, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
+    return 0;
 }
